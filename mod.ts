bce7bb0839af2c60a35699cfa96ae7260626c64d
harrysolovay <harrysolovay@gmail.com>
2022-04-08 19:10:36 -0400

    get rid of import map

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import * as hex from "std/encoding/hex.ts";
+import * as hex from "https://deno.land/std@0.134.0/encoding/hex.ts";
 import { decode as decodeRaw } from "./target/wasm/wasm_bindgen.js";
 
 export const decode = (bytes: Uint8Array) => {
